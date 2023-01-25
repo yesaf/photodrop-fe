fcbefe761619ec1432d95b2272ce53da10fcf3b4
Yevhenii Yesafiev <yesaf.eugene@gmail.com>
2023-01-25 21:14:54 +0200

    vercel: problem solving 2

--- src/components/pages/album/Album.tsx
+++ src/components/pages/album/Album.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-// import PhoneInput from 'react-phone-input-2';
+import PhoneInput from 'react-phone-input-2';
 // import PhoneField from './components/phoneField/PhoneField';
 import 'react-phone-input-2/lib/style.css';
 import styled from 'styled-components';
@@ -63,13 +63,13 @@ function Album() {
                 </header>
 
                 <AddPhoneInput className={showPhoneInput ? 'show' : ''}>
-                    {/*<div>*/}
-                    {/*    <PhoneInput*/}
-                    {/*        country={'ua'}*/}
-                    {/*        value={phone}*/}
-                    {/*        onChange={(value) => setPhone(value)}*/}
-                    {/*    />*/}
-                    {/*</div>*/}
+                    <div>
+                        <PhoneInput
+                            country={'ua'}
+                            value={phone}
+                            onChange={(value) => setPhone(value)}
+                        />
+                    </div>
                     <button onClick={() => setShowPhoneInput(false)}>
                         <img src={closeIcon} alt=""/>
                     </button>
